8d2946b8fda3f6c5a58f7f6d64408aff910910d4
Zontter <65810367+MaksimBelous@users.noreply.github.com>
2020-11-28 18:00:36 +0200

    Добавьте файлы проекта.

---
+++ MyString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Lab1._1;
+namespace Lab1._1
+{
+    public class MyString
+    {
+        private char[] symbols;
+
+        public MyString(char[] symbols)
+        {
+            this.symbols = symbols;
+        }
+
+        public char[] Value()
+        {
+            return symbols;
+        }
+        public int Lenght()
+        {
+            return symbols.Length;
+        }
+
+        ~MyString()
+        {
+            Console.WriteLine($"The {ToString()} destructor is executing.");
+            this.symbols = null;
+        }
+
+        public void Sort()
+        {
+            Array.Sort<char>(symbols, (a, b) => b.CompareTo(a));
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1._1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var example = new MyString(new char[] { '$', '%', '&', '*','?' });
+            Console.WriteLine("UNSORTED: " + new string(example.Value()));
+            example.Sort();
+            Console.WriteLine("SORTED: " + new string(example.Value()));
+        }
+    }
+}
